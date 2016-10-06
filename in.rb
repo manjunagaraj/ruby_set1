27ded5247bde8ffda5d0250bbbafe7c4035cb691
Sujina_BH <shalesh@qwinix.io>
2016-10-06 10:43:59 +0530

    assign

--- in.rb
+++ in.rb
@@ -2,10 +2,13 @@ Class Father
   def initialize  
     puts 'I am the Father.'  
   end 
-  def Thomas
-  end 
+  def fatherName(father_name)
+    @father_name=father_name
+    puts @father_name
   
-  # define fatherName --> father name is 'Thomas' . 
+  
+  # define fatherName --> father name is 'Thomas' .
+   end 
 end  
 
 
@@ -13,9 +16,12 @@ Class Mother
   def initialize  
     puts 'I am the Mother.'  
   end
-  def Martha
+  
 
   # define motherName -> mother's name is Martha
+  def motherName(mother_name)
+    @mother_name=mother_name
+    puts @mother_name
 end
 end  
 
@@ -26,6 +32,10 @@ Class Son < Father
   end
 
   # define lastName --> last name should be father's name
+  def lastName(last_name)
+    @last_name=last_name
+    puts @last_name
+  end
 
   def fullName
     # Print 'My name is Bruce Martha Thomas'
@@ -41,6 +51,9 @@ Class Son < Mother
   end 
 
   # define secondName --> second name should be Mother's name
+  def secondName(second_name)
+    @second_name=second_name
+    puts @second_name
 
   def fullName
     # Print 'My name is Bruce Martha Thomas'
@@ -50,4 +63,7 @@ end
 
 # instantiate an object and print his firstName, secondName, lastName fullName.
 object=Son.new
-object.Thomas
+object.fatherName("thomus")
+object.motherName("martha")
+object.lastName("bruce")
+object.fullName
